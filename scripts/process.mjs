9cb381c07ee8ccad679cb0c203887f9105be87ca
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-15 14:31:22 -0700

    parse data and write to data.json

---
+++ scripts/process.mjs
@@ -0,0 +1,30 @@
+import csv from "csv-parser";
+import fs from "fs";
+
+(async () => {
+  const data = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
+  let index = 0;
+
+  // todo: remove bom
+  fs.createReadStream("./cinema-data/data-csv.csv")
+    .pipe(csv())
+    .on("data", (entry) => {
+      data.features[index++] = {
+        type: "Feature",
+        properties: { ...entry },
+        geometry: {
+          type: "Point",
+          coordinates: [],
+        },
+      };
+    })
+    .on("end", () => {
+      fs.writeFile("./src/data.json", JSON.stringify(data), "utf-8", (err) => {
+        if (err) console.log(err);
+      });
+    });
+})();
