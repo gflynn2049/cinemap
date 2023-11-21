d21464761c0b699fb24da1fb8ff85722ecc8e1f7
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-21 05:19:44 -0700

    add maps

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -11,7 +11,6 @@ export const Detail = (props: {
   const { t, i18n } = useTranslation();
 
   const getProjectorsInfo = (projectorsArray: string[]) => {
-
     const getProjectorFeature = (p: string) => {
       const targetProjector = projectorsData.find(projector => projector.name == p)
       if (!targetProjector) return "";
@@ -19,10 +18,19 @@ export const Detail = (props: {
       if (features.length < 1) return ""
       if (targetProjector) return " (" + features.join(", ") + ")"
     }
-
     return projectorsArray.map((p) => p + getProjectorFeature(p)).join(', ');
 
   }
+  const mapSVG = (
+    <svg className="mr-1 w-3 h-3 text-gray-800 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 21">
+      <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+        <path d="M8 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+        <path d="M13.8 12.938h-.01a7 7 0 1 0-11.465.144h-.016l.141.17c.1.128.2.252.3.372L8 20l5.13-6.248c.193-.209.373-.429.54-.66l.13-.154Z" />
+      </g>
+    </svg>
+  )
+
+  const mapsButtonStyle = "text-sm tracking-tighter	flex justify-center items-center border-gray-300 bg-gray-100 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-500 rounded-md py-1 pr-3 pl-2"
   return (
     <div className="absolute bottom-0 max-h-96 w-full justify-center flex" onClick={() => props.setCurrent()}>
       <div
@@ -49,7 +57,7 @@ export const Detail = (props: {
                         <td className="pr-1 break-keep align-top text-gray-500 dark:text-gray-400 text-right py-1">
                           {t(key)}
                         </td>
-                        <td className="pl-2 align-top py-1 text-gray-800 dark:text-gray-200">
+                        <td className="pl-2 align-top py-1 text-gray-800 dark:text-gray-300">
                           {key === 'projectorsArray'
                             ? getProjectorsInfo(JSON.parse(value as string))
                             : value}
@@ -59,6 +67,41 @@ export const Detail = (props: {
                 )}
             </tbody>
           </table>
+          {props.current &&
+            <div className="text-center mt-5 mb-1 gap-4 sm:gap-5 md:gap-6 flex justify-center">
+              <a
+                href={`https://uri.amap.com/marker?position=${props.current.coordinates.join(',')}&name=${props.current.properties.theatre}`}
+                target="_blank"
+                rel="noreferrer"
+                className={mapsButtonStyle}>
+                {mapSVG}
+                {t("gaodeMaps")}
+              </a>
+              <a
+                href={`http://api.map.baidu.com/marker?location=${props.current.coordinates.slice().reverse().join(',')}&title=${props.current.properties.theatre}&output=html`}
+                target="_blank"
+                rel="noreferrer"
+                className={mapsButtonStyle}>
+                {mapSVG}
+                {t("baiduMaps")}
+              </a>
+              <a
+                href={`https://www.google.com/maps/search/?api=1&query=${props.current.coordinates.slice().reverse().join(',')}`}
+                target="_blank"
+                rel="noreferrer"
+                className={mapsButtonStyle}>
+                {mapSVG}
+                {t("googleMaps")}
+              </a>
+              <a
+                href={`https://maps.apple.com/?q=${props.current.coordinates.slice().reverse().join(',')}&name=${encodeURIComponent(props.current.properties.theatre)}`}
+                target="_blank"
+                rel="noreferrer"
+                className={mapsButtonStyle}>
+                {mapSVG}
+                {t("appleMaps")}
+              </a>
+            </div>}
         </div>
       </div>
     </div>
