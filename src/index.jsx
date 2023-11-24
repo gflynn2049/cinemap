149f3f22d16ca37e80670e6c28e9054e0655629e
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-24 10:38:56 -0700

    add analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { inject } from '@vercel/analytics';
 import "./i18n";
 import "./index.css";
+ 
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
